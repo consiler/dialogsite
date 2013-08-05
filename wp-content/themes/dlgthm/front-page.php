08cd72e927aa028d3a5e75d1e2407fe18390ea68
MP <consiler@gmail.com>
2013-08-04 22:07:49 -0500

    fixed wrapper template

---
+++ wp-content/themes/dlgthm/front-page.php
@@ -0,0 +1,1 @@
+a;sldkf;a
---
+++ wp-content/themes/dlgthm/wrapper.php
@@ -0,0 +1,8 @@
+<?php get_header( global_template_base() ); ?>
+
+<?php include global_template_path(); ?>
+<img src="http://www.thirdworldsuccess.com/wp-content/uploads/2011/11/THIRD-WORLD-OLDEST-VILLIAGE-SUCCESS.png">
+
+<?php get_sidebar( global_template_base() ); ?>
+
+<?php get_footer( global_template_base() ); ?>
