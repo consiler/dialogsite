8838c9b70e71b3db29f55a1def07804d8a5b1867
sanford78 <sanford78@gmail.com>
2013-08-05 11:02:50 -0500

    Working on nav

--- wp-content/themes/dlgthm/front-page.php
+++ wp-content/themes/dlgthm/front-page.php
@@ -1,3 +1,44 @@
 <div id="idelement">
+
+
+
+
+
+
+
+
+<br> 
+<br> 
+<br> 
+<br> 
+<br> 
+<br> 
+<br> 
+<br> 
+<br> 
+
+
+
+
+
+
+
+
+
+
+
+
+
   jal;sdnf;oansdfn;
+   jal;sdnf;oansdfn;
+    jal;sdnf;oansdfn;
+     jal;sdnf;oansdfn;
+      jal;sdnf;oansdfn;
+       jal;sdnf;oansdfn;
+        jal;sdnf;oansdfn;
+         jal;sdnf;oansdfn;
+          jal;sdnf;oansdfn;
+           jal;sdnf;oansdfn;
+            jal;sdnf;oansdfn;
+
 </div>
