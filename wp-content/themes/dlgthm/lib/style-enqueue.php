29e92913358e007a1811be26c1c15c17bf7d9606
sanford78 <sanford78@gmail.com>
2013-08-05 17:43:49 -0500

    organizing php libraries

---
+++ wp-content/themes/dlgthm/lib/style-enqueue.php
@@ -0,0 +1,26 @@
+/*
+ *-------------------------------------------------------
+ *
+ *                    Additional Styles
+ *
+ *-------------------------------------------------------
+*/
+
+function theme_styles()  
+{ 
+  // Register the style like this for a theme:  
+  // (First the unique name for the style (custom-style) then the src, 
+  // then dependencies and ver no. and media type)
+  wp_register_style( 'app-style', 
+    get_bloginfo('template_url').'/css/main.min.css', 
+    array(), 
+    '0.1', 
+    'all'
+    //optionally screen, handheld, print
+    );
+
+  // enqueing the style:
+  wp_enqueue_style( 'app-style' );
+}
+//adding the hook
+add_action('wp_enqueue_scripts', 'theme_styles');
