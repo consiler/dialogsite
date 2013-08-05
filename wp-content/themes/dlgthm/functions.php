93686b25e4a08a455ab6faf74c00565e82693c56
MP <consiler@gmail.com>
2013-08-04 19:22:17 -0500

    dis da latest commit

--- wp-content/themes/dlgthm/functions.php
+++ wp-content/themes/dlgthm/functions.php
@@ -524,3 +524,43 @@ function twentythirteen_customize_preview_js() {
 	wp_enqueue_script( 'twentythirteen-customizer', get_template_directory_uri() . '/js/theme-customizer.js', array( 'customize-preview' ), '20130226', true );
 }
 add_action( 'customize_preview_init', 'twentythirteen_customize_preview_js' );
+
+function my_template_path() {
+	return My_Wrapping::$main_template;
+}
+
+function my_template_base() {
+	return My_Wrapping::$base;
+}
+
+
+class My_Wrapping {
+
+	/**
+	 * Stores the full path to the main template file
+	 */
+	static $main_template;
+
+	/**
+	 * Stores the base name of the template file; e.g. 'page' for 'page.php' etc.
+	 */
+	static $base;
+
+	static function wrap( $template ) {
+		self::$main_template = $template;
+
+		self::$base = substr( basename( self::$main_template ), 0, -4 );
+
+		if ( 'index' == self::$base )
+			self::$base = false;
+
+		$templates = array( 'wrapper.php' );
+
+		if ( self::$base )
+			array_unshift( $templates, sprintf( 'wrapper-%s.php', self::$base ) );
+
+		return locate_template( $templates );
+	}
+}
+
+add_filter( 'template_include', array( 'My_Wrapping', 'wrap' ), 99 );
---
+++ wp-content/themes/dlgthm/wrap.php
@@ -0,0 +1,8 @@
+<?php get_header( my_template_base() ); ?>
+
+<?php include my_template_path(); ?>
+<img src="http://www.thirdworldsuccess.com/wp-content/uploads/2011/11/THIRD-WORLD-OLDEST-VILLIAGE-SUCCESS.png">
+
+<?php get_sidebar( my_template_base() ); ?>
+
+<?php get_footer( my_template_base() ); ?>
