c12b7d9933e2796f0eef6d752775b9b29fc27652
MP <consiler@gmail.com>
2013-08-06 15:01:18 -0500

    deleted custom title script, already taken care of by Yoast SEO plugin ;

--- wp-content/themes/dlgthm/functions.php
+++ wp-content/themes/dlgthm/functions.php
@@ -106,38 +106,6 @@ function dialog_scripts_styles() {
 add_action( 'wp_enqueue_scripts', 'dialog_scripts_styles' );
 
 /**
- * Creates a nicely formatted and more specific title element text for output
- * in head of document, based on current view.
- *
- * @since Twenty Thirteen 1.0
- *
- * @param string $title Default title text for current view.
- * @param string $sep Optional separator.
- * @return string The filtered title.
- */
-function twentythirteen_wp_title( $title, $sep ) {
-	global $paged, $page;
-
-	if ( is_feed() )
-		return $title;
-
-	// Add the site name.
-	$title .= get_bloginfo( 'name' );
-
-	// Add the site description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		$title = "$title $sep $site_description";
-
-	// Add a page number if necessary.
-	if ( $paged >= 2 || $page >= 2 )
-		$title = "$title $sep " . sprintf( __( 'Page %s', 'twentythirteen' ), max( $paged, $page ) );
-
-	return $title;
-}
-add_filter( 'wp_title', 'twentythirteen_wp_title', 10, 2 );
-
-/**
  * Registers two widget areas.
  *
  * @since Twenty Thirteen 1.0
@@ -449,8 +417,6 @@ function twentythirteen_customize_preview_js() {
 }
 add_action( 'customize_preview_init', 'twentythirteen_customize_preview_js' );
 
-//$libdir = "wp-content/themes/dlgthm/lib/";
 $libdir = "lib/";
 require_once(locate_template($libdir.'template-wrapper.php'));
 require_once(locate_template($libdir.'cpt.php'));
-require_once(locate_template($libdir.'style-enqueue.php'));
