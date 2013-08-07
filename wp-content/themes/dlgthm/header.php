c95886abce8ac4a4ef1e41e1c12724425d995e97
sanford78 <sanford78@gmail.com>
2013-08-07 01:11:43 -0500

    Added bare-bones default value nav structure

--- wp-content/themes/dlgthm/header.php
+++ wp-content/themes/dlgthm/header.php
@@ -17,5 +17,28 @@
     <![endif]-->
     <?php wp_head(); ?>
   </head>
+<?php
 
+$defaults = array(
+  'theme_location'  => 'Navigation Menu',
+  'menu'            => '',
+  'container'       => 'div',
+  'container_class' => '',
+  'container_id'    => '',
+  'menu_class'      => 'menu',
+  'menu_id'         => '',
+  'echo'            => true,
+  'fallback_cb'     => 'wp_page_menu',
+  'before'          => '',
+  'after'           => '',
+  'link_before'     => '',
+  'link_after'      => '',
+  'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+  'depth'           => 0,
+  'walker'          => ''
+);
+
+wp_nav_menu( $defaults );
+
+?>
   <body <?php body_class(); ?>>
