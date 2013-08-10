dfb0bdeb5eaf144fd7f8f9d4f8c9f3e104ed88e7
MP <consiler@gmail.com>
2013-08-10 15:43:27 -0500

    added sitemap notes to readme

--- wp-content/themes/dlgthm/footer.php
+++ wp-content/themes/dlgthm/footer.php
@@ -1,4 +1,13 @@
   </div>
+<div class="footer-wrap">
+  <div class="footer-inner">
+    <div class="footer-menu-wrap">
+      <?php
+      wp_nav_menu( $defaults );
+      ?>
+    </div>
+  </div>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
