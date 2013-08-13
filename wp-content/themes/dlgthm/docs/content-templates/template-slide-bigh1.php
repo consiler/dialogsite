3cba1203a13af4f8f94e35a6ec8cc26a30c777bf
MP <consiler@gmail.com>
2013-08-13 03:49:29 -0500

    Now when templates are rendered, they check to see if their assigned fieldset has all of the fields required by the template

--- wp-content/themes/dlgthm/docs/content-templates/template-slide-bigh1.php
+++ wp-content/themes/dlgthm/docs/content-templates/template-slide-bigh1.php
@@ -1,5 +1,18 @@
-<div class="block-wrap" style="background-color: <?php the_field('background-color'); ?>;">
+<?php
+/*
+Template: FS Slide Big H1
+Comment: A template for a basic fullwidth slide with arbitrary bg color and text color. Text is bolded, center.
+*/
+$required_fields = array(
+  'background-color',
+  'color',
+  'big_centered_title_text');
+$fieldset = verified_fieldset($required_fields);
+if($fieldset) {
+?>
+<div class="block-wrap" style="background-color: <?php echo $fieldset['background-color']; ?>;">
   <div class="block-center">
-    <h1 style="color: <?php the_field('color'); ?>;"><?php the_field('big_centered_title_text'); ?></h1>
+    <h1 style="color: <?php echo $fieldset['color']; ?>;"><?php echo $fieldset['big_centered_title_text']; ?></h1>
   </div>
 </div>
+<?php } else { echo('Assigned fieldset does not match template fieldset in CPT template slide-bigh1.'); } ?>
--- wp-content/themes/dlgthm/lib/cpt-renderer.php
+++ wp-content/themes/dlgthm/lib/cpt-renderer.php
@@ -3,15 +3,18 @@
 function the_cpt_post()
 {
   $template_id = get_field('template_term');
-  if(count($template_id) < 1)
+  if($template_id)
   {
-    die('You cannot have multiple templates or fieldsets assigned to a single post. Look in cpt-renderer to debug.');
-  } else {
-    $template_term = get_term($template_id[0], CPT_TEMPLATE_TAX_NAME);
-    $templates = CONTENT_TEMPLATE_PATH.'template-'.$template_term->name.'.php';
-    echo '<div class="content-template-wrap template-'.$template_term->name.'">';
-    require locate_template($templates);
-    echo '</div>';
+    if(count($template_id) < 1)
+    {
+      die('You cannot have multiple templates or fieldsets assigned to a single post. Look in cpt-renderer to debug.');
+    } else {
+      $template_term = get_term($template_id[0], CPT_TEMPLATE_TAX_NAME);
+      $templates = CONTENT_TEMPLATE_PATH.'template-'.$template_term->name.'.php';
+      echo '<div class="content-template-wrap template-'.$template_term->name.'">';
+      require locate_template($templates);
+      echo '</div>';
+    }
   }
 }
 
@@ -31,5 +34,21 @@ function the_page_cpt()
     }
     wp_reset_postdata();
   }
+}
+
+// This matches an array of field names with the actual fields assigned to the post.
+// If the assigned fieldset has all the fields we need for the template, we return them.
+// Otherwise, we just return false to indicate that the fieldset is missing fields required by the template.
+function verified_fieldset($expected_fieldset)
+{
+  $assigned_fieldset = get_fields();
+  foreach($expected_fieldset as $expected_field)
+  {
+    if(!in_array($expected_field, array_keys($assigned_fieldset)))
+    {
+      return false;
+    }
+  }
+  return $assigned_fieldset;
 }
 ?>
