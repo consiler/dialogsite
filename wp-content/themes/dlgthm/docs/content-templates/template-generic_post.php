b16aed6ae07b3f34222d2386667f129736ee96a6
MP <consiler@gmail.com>
2013-08-27 21:13:53 -0400

    getting sam udt

--- wp-content/themes/dlgthm/docs/content-templates/template-generic_post.php
+++ wp-content/themes/dlgthm/docs/content-templates/template-generic_post.php
@@ -9,7 +9,7 @@ if($fieldset) {
 ?>
 <div class="generic_post">
   <div class="spyOnMe" id="content-section-<?php the_ID(); ?>">
-    <h1><?php the_field('title'); ?></h1>
+    <h1 class="generic_post_h1"><?php the_field('title'); ?></h1>
     <?php the_field('content'); ?>
   </div>
 </div>
