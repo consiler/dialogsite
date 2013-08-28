915c15653a4f15ceee2fb5bd6446607b3e14d069
MP <consiler@gmail.com>
2013-08-27 23:03:43 -0400

    added dark grey header to generic_post pages

--- wp-content/themes/dlgthm/docs/wrapper/wrapper.php
+++ wp-content/themes/dlgthm/docs/wrapper/wrapper.php
@@ -1,21 +1,22 @@
-<?php get_header( global_template_base() ); ?>
-<?php
+<?php get_header( global_template_base() ); 
 $url_field = get_field('hero_banner');
-if(get_field('hero_banner'))
-{
 ?>
-<div id="banner-wrap">
-  <div id="banner-parallax-control">
-  </div>
-   <div id="banner" data-custom-background-image="<?php echo $url_field['url']; ?>" data-stellar-ratio="2" data-stellar-vertical-offset="95" data-stellar-offset-parent="true">
-  </div>
-  <div id="banner-bg-filler"></div>
+<div id="banner-wrap" class="<?php if(!$url_field) { echo 'nohero'; } ?>">
+  <?php
+  if($url_field)
+  {
+  ?>
+    <div id="banner-parallax-control">
+    </div>
+    <div id="banner" data-custom-background-image="<?php echo $url_field['url']; ?>" data-stellar-ratio="2" data-stellar-vertical-offset="95" data-stellar-offset-parent="true">
+    </div>
+    <div id="banner-bg-filler"></div>
+  <?php } ?>
   <div id="page-heading-wrap">
     <div id="page-heading-inner">
       <h1 id="page-heading"><?php the_title(); ?></h1>
     </div>
   </div>
 </div>
-<?php } ?>
   <?php include global_template_path(); ?>
   <?php get_footer( global_template_base() ); ?>
