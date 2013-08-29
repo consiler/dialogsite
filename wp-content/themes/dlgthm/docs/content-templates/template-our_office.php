d85a1e9e4239c24fc49b0867963388a0441ec1ab
MP <consiler@gmail.com>
2013-08-29 01:51:56 -0400

    fixed team bug

--- wp-content/themes/dlgthm/docs/content-templates/template-our_office.php
+++ wp-content/themes/dlgthm/docs/content-templates/template-our_office.php
@@ -3,13 +3,7 @@
 Template: Office Member Bios
 Comment: A template for a list/grid of people in the office
 */
-$required_fields = array();
-$fieldset = verified_fieldset($required_fields);
-if($fieldset) {
 ?>
 <div class="our_office">
-   <?php render_cpt_with_template('our_office', 'our_office'); ?>
+   <?php render_cpt_with_template('our_office', 'our_office_person'); ?>
 </div>
-<?php } else {
-  fieldset_mismatch_error($required_fields);
-} ?>
