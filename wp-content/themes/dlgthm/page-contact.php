600154dc0c1a70ff2c6510db6126f8018818e098
MP <consiler@gmail.com>
2013-08-29 01:09:31 -0400

    contact page stuff

--- wp-content/themes/dlgthm/page-contact.php
+++ wp-content/themes/dlgthm/page-contact.php
@@ -1,10 +1,10 @@
 <?php
 /* Template Name: Contact Page */
 ?>
-<div class="content-wrap">
+<div class="content-wrap content-wrap-pad-top">
   <div class="content-body <?php if(!$scrollspy) { echo 'content-body-fullwidth'; } ?>">
     <?php
       the_content();
-    ?>
+    ?>asdfasdf
   </div>
 </div>
