4bd067b2fbb76408839e243f1151fd75d173e4b1
sanford78 <sanford78@gmail.com>
2013-08-29 01:43:47 -0400

    errors

--- wp-content/themes/dlgthm/docs/content-templates/template-our_office_person.php
+++ wp-content/themes/dlgthm/docs/content-templates/template-our_office_person.php
@@ -3,13 +3,16 @@
 Template: Office Member Bios
 Comment: A template for a list/grid of people in the office
 */
-$required_fields = array();
+$required_fields = array('grid_person_name', 'grid_person_image');
 $fieldset = verified_fieldset($required_fields);
 if($fieldset) {
 ?>
-<div class="our_office">
-   <?php render_cpt_with_template('our_office', 'our_office'); ?>
-</div>
+<div class="our_office_person">
+   <?php
+    $name = get_field("grid_person_name");
+    var_dump($name);
+   ?>
+ </div>
 <?php } else {
   fieldset_mismatch_error($required_fields);
 } ?>
