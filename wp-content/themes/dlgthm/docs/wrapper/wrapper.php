aca4ed50a161811c8a6a60858935eb08b387cb57
MP <consiler@gmail.com>
2013-08-31 22:22:44 -0400

    frontpage stuff

--- wp-content/themes/dlgthm/docs/wrapper/wrapper.php
+++ wp-content/themes/dlgthm/docs/wrapper/wrapper.php
@@ -11,12 +11,13 @@ $url_field = get_field('hero_banner');
     <div id="banner" data-custom-background-image="<?php echo $url_field['url']; ?>" data-stellar-ratio="2" data-stellar-vertical-offset="95" data-stellar-offset-parent="true">
     </div>
     <div id="banner-bg-filler"></div>
-  <?php } ?>
   <div id="page-heading-wrap">
     <div id="page-heading-inner">
       <h1 id="page-heading"><?php the_title(); ?></h1>
     </div>
   </div>
+  <?php } ?>
+  
 </div>
   <?php include global_template_path(); ?>
   <?php get_footer( global_template_base() ); ?>
--- wp-content/themes/dlgthm/page-contact.php
+++ wp-content/themes/dlgthm/page-contact.php
@@ -1,20 +1,6 @@
 <?php
 /* Template Name: Contact Page */
 ?>
-<div class="content-wrap content-wrap-pad-top">
-  <div class="content-body <?php if(!$scrollspy) { echo 'content-body-fullwidth'; } ?>">
-    <div class="contact-top">
-      Banner will go here.
-    </div>
-    <div class="contact-wrap">
-      <div class="contact-left">
-        <p>Thank you for your interest in Dialog.</p>
-      </div>
-      <div class="contact-right">
-        <div class="contact-form-wrap">
-          <?php if(function_exists('ninja_forms_display_form')){ ninja_forms_display_form(1); } ?>
-        </div>
-      </div>
-    </div>
-  </div>
+<div class="contact-main">
+  <?php if(function_exists('ninja_forms_display_form')){ ninja_forms_display_form(1); } ?>
 </div>
